3aa9c037d4ad7446c50e7c02aab856e1231c984e
Sebastian Jiro Schlecht <sebastianjiroschlecht@gmail.com>
2023-01-06 22:09:32 +0200

    rework fig 6 and 7

--- paraunitary_coupling_example.m
+++ paraunitary_coupling_example.m
@@ -4,7 +4,7 @@ addpath('plot/.');
 addpath('utils/.');
 close all;
 
-%% design coupling matrix
+%% Set parameters
 
 Nrooms = 3;
 % aperture size connecting the three rooms - assumed to be symmetric
@@ -25,21 +25,20 @@ win_len = zeros(Nrooms,Nrooms);
 Nfft = 1024;
 w = linspace(0, pi, Nfft/2);
 
-
+%% Compute initial diffraction filter
 for i = 1:Nrooms
-
     for j = 1:Nrooms
-       coupling_areas(i,j) = absorp_coef(i).*area(i) + (pi*aperture(i,j)^2).*(1-absorp_coef(i));
+        coupling_areas(i,j) = absorp_coef(i).*area(i) + (pi*aperture(i,j)^2).*(1-absorp_coef(i));
 
-        if (i == j)
-            continue;
-        else
+        if (i ~= j)
             [b_diff, a_diff, win_len(i,j)] = design_diffraction_filter(aperture(i,j),fs,c,des);
             filter_coefs(i,j,1:win_len(i,j)) =  b_diff;
-
         end
     end
 end
+
+%% Compute initial coupling matrix
+
 degree = max(max(win_len));
 couplingMatrix = zeros(Nrooms, Nrooms, degree);
 const = max(max(max(filter_coefs)));
@@ -50,9 +49,7 @@ signVec = zeros(1,1,degree);
 
 for i = 1:Nrooms
     for j = 1:Nrooms
-        if ( i == j)
-            continue;
-        else
+        if ( i ~= j)
             shift = (win_len(i,j)+1)/2;
             couplingMatrix(i,j,midPoint-shift+1:midPoint+shift-1) = ...
                 (pi*(aperture(i,j)^2))/sqrt(coupling_areas(i,j)*coupling_areas(j,i)) ...
@@ -72,39 +69,43 @@ originalMatrix_freq = fft(originalMatrix, Nfft, 3);
 
 %% PRODUCES FIGURE 6A
 % plot time domain filters
-figure('Units','inches', 'Position',[0 0 2.9 2.5],'PaperPositionMode','auto');
-[~, pHDes] = plotImpulseResponseMatrix(0:(degree-1), originalMatrix, 'save', 1, ...
-   'colors', [0, 0.4470, 0.7410],'ylim', [-0.01,0.01], 'title', 'Initial matrix', ...
-   'stemFlag', 0, 'xlabel', 'Samples', 'ylabel', 'Amplitude'); hold on;
-print('figures/3x3_couplingMatrix_init.eps', '-depsc');
+figure('Units','inches', 'Position',[0 0 4 3],'PaperPositionMode','auto'); hold on;
+[plotAxes, pHDes, commonAx] = plotImpulseResponseMatrix(0:(degree-1), originalMatrix, ...
+    'Color', [0, 0.4470, 0.7410],'ylim', [-0.01,0.01], ...
+    'xlabel', 'Time (Samples)', 'ylabel', 'Amplitude'); 
+commonAx.YLabel.Position(1) = -0.1;
+set(plotAxes,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+set(commonAx,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+exportgraphics(gcf, 'figures/3x3_couplingMatrix_init.png', 'Resolution', 300)
 
-
-figure('Units','inches', 'Position',[0 0 2.9 2.5],'PaperPositionMode','auto');
-[~, perFreqProcrustes] = plotImpulseResponseMatrix(0:(degree-1), couplingMatrix, ...
-    'save', 1, 'colors', [0.8500, 0.3250, 0.0980], 'ylim', [-0.01,0.01] , ...
-    'title', 'Per-Frequency Procrustes', 'stemFlag',0, ...
-    'xlabel', 'Samples', 'ylabel', 'Amplitude'); 
-print('figures/3x3_couplingMatrix_pFProc.eps', '-depsc');
-
+figure('Units','inches', 'Position',[0 0 4 3],'PaperPositionMode','auto'); hold on;
+[plotAxes, perFreqProcrustes, commonAx] = plotImpulseResponseMatrix(0:(degree-1), couplingMatrix, ...
+    'Color', [0.8500, 0.3250, 0.0980], 'ylim', [-0.01,0.01] , ...
+    'xlabel', 'Time (Samples)', 'ylabel', 'Amplitude');
+commonAx.YLabel.Position(1) = -0.1;
+set(plotAxes,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+set(commonAx,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+exportgraphics(gcf, 'figures/3x3_couplingMatrix_pFProc.png', 'Resolution', 300)
 
 %% PRODUCES FIGURE 7
 % plot frequency response
 
-figure('Units','inches', 'Position',[0 0 6.6 2.5],'PaperPositionMode','auto');
+figure('Units','inches', 'Position',[0 0 4 2.5],'PaperPositionMode','auto'); hold on;
 [pADes, pHDes] = plotImpulseResponseMatrix((w/pi)*(fs/2), ...
-    20*log10(abs(originalMatrix_freq(:,:,1:Nfft/2))), 'xlogaxis',1, 'xlim',[20,20000],...
-    'xlabel', 'Frequency (Hz)', 'ylabel', 'Magnitude (dB)', 'stemFlag',0, ...
-    'save', 1, 'plotYLabels', 0); hold on;
-[pAProcrustes, pProcrustes] = plotImpulseResponseMatrix((w/pi)*(fs/2), ...
-    20*log10(abs(couplingMatrix_freq(:,:,1:Nfft/2))), 'xlogaxis',1, 'xlim',[20,20000],...
-     'xlabel', 'Frequency (Hz)', 'ylabel', 'Magnitude (dB)', 'stemFlag',0, ...
-     'save',1, 'commonYAxis', 1, 'ylim', [-80,5]); hold off;
-
+    mag2db(abs(originalMatrix_freq(:,:,1:Nfft/2))), ...
+    'xlim',[200,20000],'xlabel', 'Frequency (Hz)', 'ylabel', 'Magnitude (dB)');
+[pAProcrustes, pProcrustes,commonAx] = plotImpulseResponseMatrix((w/pi)*(fs/2), ...
+    mag2db(abs(couplingMatrix_freq(:,:,1:Nfft/2))), ...
+    'xlim',[200,20000],'xlabel', 'Frequency (Hz)', 'ylabel', 'Magnitude (dB)','ylim', [-80,5]);
+set(pAProcrustes,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+set(commonAx,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
+set(pADes,'XScale','log');
 
 % add legend
-Lgnd = legend(pADes(1,1), [pHDes(1,1), pProcrustes(1,1)], 'Desired', 'PF Procrustes', ...
-    'Location', 'southeast');
-% print('figures/3x3_couplingMatrix_resp_compared.eps', '-depsc');
+Lgnd = legend(pADes(1,1), [pHDes(1,1), pProcrustes(1,1)], 'Desired', 'PF Procrustes');
+Lgnd.NumColumns = 2;
+Lgnd.Position(1:2) = [0.33 0.94];
+exportgraphics(gcf, 'figures/3x3_couplingMatrix_resp_compared.png', 'Resolution', 300)
 
 
 %% PRODUCES FIGURE 6B
@@ -120,7 +121,7 @@ pink = [255, 192, 203]/255;
 color_gradient(:,:,2) = create_color_gradient(red, pink, Nrooms);
 
 
-figure('Units','inches', 'Position',[0 0 2.9 2.5],'PaperPositionMode','auto'); grid on;
+figure('Units','inches', 'Position',[0 0 4 2],'PaperPositionMode','auto'); grid on;
 S = svdPerBin(fft(originalMatrix,2*degree,3));
 p1 = semilogx(linspace(-fs/2,fs/2,2*degree), 20*log10(abs(S')));grid on;
 xlabel('Frequency (Hz)');
@@ -132,10 +133,9 @@ set(p1, NameArray, ValueArray);
 xlim([0,fs/2]);
 axis tight;
 set(gca,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
-print('figures/3x3_couplingMatrix_SVD_init.eps', '-depsc');
+exportgraphics(gcf, 'figures/3x3_couplingMatrix_SVD_init.png', 'Resolution', 300)
 
-
-figure('Units','inches', 'Position',[0 0 2.9 2.5],'PaperPositionMode','auto'); grid on;
+figure('Units','inches', 'Position',[0 0 4 2],'PaperPositionMode','auto'); grid on;
 S = svdPerBin(fft(couplingMatrix,2*degree,3));
 p2 = semilogx(linspace(-fs/2,fs/2,2*degree), 20*log10(abs(S'))); grid on;
 xlabel('Frequency (Hz)'); ylabel('Singular Values (dB)');
@@ -146,22 +146,22 @@ set(p2, NameArray, ValueArray);
 axis tight;
 xlim([0, fs/2]);
 set(gca,'FontUnits','points', 'FontWeight','normal', 'FontSize',8, 'FontName','Times');
-% print('figures/3x3_couplingMatrix_SVD_pFProc.eps', '-depsc');
+exportgraphics(gcf, 'figures/3x3_couplingMatrix_SVD_pFProc.png', 'Resolution', 300)
 
 
 %% helper functions
 
 function PP = svdPerBin(TT)
 
-    PP = zeros(size(TT,[1 3]));
-    for f = 1:size(TT,3)
-       s = (svd(TT(:,:,f)));  
-       PP(:,f) = s;
-    end
+PP = zeros(size(TT,[1 3]));
+for f = 1:size(TT,3)
+    s = (svd(TT(:,:,f)));
+    PP(:,f) = s;
+end
 end
 
 
 function [color_gradient] = create_color_gradient(col1, col2, nterms)
-    color_gradient = [linspace(col1(1),col2(1),nterms)', linspace(col1(2),col2(2),nterms)'...
-        , linspace(col1(3),col2(3),nterms)'];
+color_gradient = [linspace(col1(1),col2(1),nterms)', linspace(col1(2),col2(2),nterms)'...
+    , linspace(col1(3),col2(3),nterms)'];
 end
